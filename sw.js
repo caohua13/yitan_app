23a50c69415ff5cd51e40867c08c9f77dfe48e49
caohua13 <3430433207@qq.com>
2026-07-09 21:10:12 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'yitan-v1'
+const urlsToCache = [
+  '/yitan_app/index.html',
+  '/yitan_app/manifest.json'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+    })
+  )
+})
